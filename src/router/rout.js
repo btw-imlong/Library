ff95b33e5fd6aa51e83a747aaa50ca8eff616db4
Rachana <soum.dymonyrachana@institute.pse.ngo>
2025-06-16 10:03:31 +0700

    implement list book

--- src/router/rout.js
+++ src/router/rout.js
@@ -2,10 +2,12 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/student.vue";
 import Login from "../auth/login.vue";
 import Book from "../views/book.vue";
+import listb from "@/views/listb.vue";
 const routes = [
   { path: "/", name: "Home", component: Home },
   { path: "/about", name: "About", component: Login },
   { path: "/book", name: "Book", component: Book },
+  { path: "/listb", name: "Listb", component: listb },
 ];
 
 const router = createRouter({
